Fixed gradient calculation and added bfgs optimization

logistic-regression/cross_entropy_loss.py
@@ -2,10 +2,19 @@ import numpy as np
 from sigmoid import *
 
 # Cost function for logistic regression
-def cross_entropy_loss(X, y, theta):
+def cross_entropy_loss(theta, X, y):
+    m = X.shape[0]
 
     h = sigmoid(np.dot(X, theta))
-    J = - np.sum(y * np.log(h) + (1 - y) * np.log(1 - h))
-    grad = np.dot(np.transpose(X), h - y)
+    J = np.sum(-y * np.log(h) - (1 - y) * np.log(1 - h)) / 500
 
-    return J, grad
+    return J
+
+# Gradient of the loss function
+def cross_entropy_gradient(theta, X, y):
+    m = X.shape[0]
+
+    h = sigmoid(np.dot(X, theta))
+    grad = np.dot(np.transpose(X), h - y.reshape((m, 1))) / m
+
+    return grad