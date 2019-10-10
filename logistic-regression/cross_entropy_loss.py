e2e4c198e7956d61682314d2eb719d028df5bd3b
kdan5 <kdan5@vt.edu>
2019-10-09 23:54:29 -0400

    Fixed loss calculation and refactored gradient code

--- logistic-regression/cross_entropy_loss.py
+++ logistic-regression/cross_entropy_loss.py
@@ -4,17 +4,18 @@ from sigmoid import *
 # Cost function for logistic regression
 def cross_entropy_loss(theta, X, y):
     m = X.shape[0]
-
+    y = y.reshape((100,1))
     h = sigmoid(np.dot(X, theta))
-    J = np.sum(-y * np.log(h) - (1 - y) * np.log(1 - h)) / 500
+    J = np.sum(-y * np.log(h) - (1 - y) * np.log(1 - h)) / m
 
     return J
 
 # Gradient of the loss function
 def cross_entropy_gradient(theta, X, y):
     m = X.shape[0]
-
+    y = y.reshape((100,1))
     h = sigmoid(np.dot(X, theta))
-    grad = np.dot(np.transpose(X), h - y.reshape((m, 1))) / m
+    grad = np.dot(np.transpose(X), h - y) / m
+    grad = np.ndarray.flatten(grad)
 
     return grad
