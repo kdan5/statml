Fixed gradient function to comply with scipy minimize and refactored plotting code

logistic-regression/cross_entropy_loss.py
@@ -3,8 +3,9 @@ from sigmoid import *
 
 # Cost function for logistic regression
 def cross_entropy_loss(theta, X, y):
-    m = X.shape[0]
-    y = y.reshape((100,1))
+    m, n = X.shape
+    y = y.reshape((m, 1))
+    theta = theta.reshape((n, 1))
     h = sigmoid(np.dot(X, theta))
     J = np.sum(-y * np.log(h) - (1 - y) * np.log(1 - h)) / m
 
@@ -12,8 +13,9 @@ def cross_entropy_loss(theta, X, y):
 
 # Gradient of the loss function
 def cross_entropy_gradient(theta, X, y):
-    m = X.shape[0]
-    y = y.reshape((100,1))
+    m, n = X.shape
+    y = y.reshape((m, 1))
+    theta = theta.reshape((n, 1))
     h = sigmoid(np.dot(X, theta))
     grad = np.dot(np.transpose(X), h - y) / m
     grad = np.ndarray.flatten(grad)

logistic-regression/plot_class.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Plot 2-dimensional data that is split between two classes
 def plot_class(X, y):
+    # Identify the indices by their classification
     pos = np.where(y == 1)[0]
     neg = np.where(y == 0)[0]
 
-    plt.scatter(X[pos, 0], X[pos, 1], marker='+')
-    plt.scatter(X[neg, 0], X[neg, 1], marker='.')
-    plt.show()
+    # Plot the data
+    plt.scatter(X[pos, 0], X[pos, 1], marker='+', c='r')
+    plt.scatter(X[neg, 0], X[neg, 1], marker='.', c='b')

logistic-regression/plot_decision_boundary.py
@@ -1,21 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from plot_class import *
 from map_feature import *
 
 # Plot the decision boundary of the logistic regression algorithm
-def plot_decision_boundary(X, y, theta):
-    # Plot the data 
-    plot_class(X[:,1:2], y)
+def plot_decision_boundary(theta, X, y):
 
-    if (X.size[1] <= 3):
-        x_points = [np.min(X[:, 1]) - 2, np.max(X[:, 1]) + 2]
-        y_points = (-1 / theta[2]) * (theta[1] * x_points) + theta[0]
-
+    if (X.shape[1] <= 3):
+        x_points = np.array([np.min(X[:, 1]), np.max(X[:, 1])])
+        y_points = - (theta[0] + (theta[1] * x_points)) / theta[2]
+        
         plt.plot(x_points, y_points)
-
-        plt.legend('Admitted', 'Not Admitted', 'Decision Boundary')
-        plt.axis([30, 100, 30, 100])
     else:
         u = np.linspace(-1, 1.5, 50)
         v = np.linspace(-1, 1.5, 50)