298f0a0298b2d6fa893c5fb6b127c7d6feeb02fc
Daniel Kutney <kdan5@vt.edu>
2019-10-16 23:56:02 -0400

    Fixed bug in map_feature

--- logistic-regression/map_feature.py
+++ logistic-regression/map_feature.py
@@ -1,14 +1,12 @@
 import numpy as np
-
-# Maps features to polynomial features
-# Returns a feature vector containing 1, X1, X2, X1^2, X2^2, X1*X2, X1*X2^2, ...
+'''
+Maps features to polynomial features.
+Returns a feature vector containing 1, X1, X2, X1^2, X2^2, X1*X2, X1*X2^2, ...
+'''
 def map_feature(X1, X2):
     degree = 6
-    s = (X1[:, 1].shape[0], np.sum(range(8)))
-    output = [1] * s
-
-    for i in range(1, degree):
-        for j in range(i):
-            output.append(np.power(X1, i - j) * np.power(X2, j))
-    
-    return output
+    out = np.ones(X1.shape[0])[:,np.newaxis]
+    for i in range(1, degree+1):
+        for j in range(i+1):
+            out = np.hstack((out, np.multiply(np.power(X1, i-j), np.power(X2, j))[:,np.newaxis]))
+    return out
