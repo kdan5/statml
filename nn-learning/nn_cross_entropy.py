3ade66075d4e3026f379c7ba527c4f784aaf312e
Daniel Kutney <kdan5@vt.edu>
2019-10-27 23:53:21 -0400

    Added regularization to loss function

--- nn-learning/nn_cross_entropy.py
+++ nn-learning/nn_cross_entropy.py
@@ -3,15 +3,15 @@ from sigmoid import *
 
 def nn_cross_entropy_loss(params, input_layer_size, hidden_layer_size, labels, X, y, Lambda):
     m, n = X.shape
-    theta1 = params['theta1']
-    theta2 = params['theta2']
+    theta1 = params['Theta1']
+    theta2 = params['Theta2']
 
     theta1_grad = np.zeros(theta1.shape)
     theta2_grad = np.zeros(theta2.shape)
 
     a1 = np.concatenate((np.ones((m, 1)), X), -1)
     z2 = np.dot(a1, np.transpose(theta1))
-    a2 = np.concatenate((np.ones((m, 1)), sigmoid(z2), -1)
+    a2 = np.concatenate((np.ones((m, 1)), sigmoid(z2), -1))
     z3 = np.dot(a2, np.transpose(theta2))
     a3 = sigmoid(z3)
 
@@ -20,4 +20,8 @@ def nn_cross_entropy_loss(params, input_layer_size, hidden_layer_size, labels, X
     for i in range(m):
         y_mat[i, y[i]] = 1
 
-    J = -np.sum(np.sum(y_mat * np.log(a3) + (1 - y_mat) * np.log(1 - a3))) / m
+    loss = -np.sum(np.sum(y_mat * np.log(a3) + (1 - y_mat) * np.log(1 - a3))) / m
+    reg_term = (np.sum(np.sum(np.power(theta1[:, 1:], 2))) + np.sum(np.sum(np.power(theta2[:, 1:], 2)))) * Lambda / m
+    J = loss + reg_term
+
+    return J
