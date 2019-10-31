12f7e4e05ec188a0210cd92ef1d835e866a792d8
Daniel Kutney <kdan5@vt.edu>
2019-10-31 00:00:31 -0400

    Added sigmoid gradient

--- nn-learning/sigmoid.py
+++ nn-learning/sigmoid.py
@@ -5,3 +5,10 @@ Compute the sigmoid function with the given input.
 '''
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
+
+'''
+Compute the gradient of the sigmoid function.
+'''
+def sigmoid_grad(z):
+    return sigmoid(z) * (1 - sigmoid(z))
