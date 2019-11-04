a4f284311f16ce53708abd0f5b11d03ea4270116
Daniel Kutney <kdan5@vt.edu>
2019-11-03 20:02:34 -0500

    Changed gradient calculation

--- nn-learning/nn_cross_entropy.py
+++ nn-learning/nn_cross_entropy.py
@@ -19,9 +19,8 @@ def nn_cross_entropy(params, input_layer_size, hidden_layer_size, labels, X, y, 
 
     for i in range(m):
         y_mat[i, y[i]] = 1
-    p = -y_mat * np.log(a3)
-    n = (1 - y_mat) * np.log(1 - a3)
-    loss = np.sum(np.sum(p - n)) / m
+
+    loss = np.sum(np.sum((-y_mat * np.log(a3)) - ((1 - y_mat) * np.log(1 - a3)))) / m
     print("loss:", loss)
     reg_term = (np.sum(np.sum(np.power(theta1[:, 1:], 2))) + np.sum(np.sum(np.power(theta2[:, 1:], 2)))) * Lambda / m
     J = loss + reg_term
@@ -29,13 +28,13 @@ def nn_cross_entropy(params, input_layer_size, hidden_layer_size, labels, X, y, 
     for t in range(m):
         a_1 = np.concatenate((np.ones((1, 1)), X[t, :].reshape(n, 1)))
         print(a_1.shape)
-        z_2 = np.multiply(theta1, a_1)
+        z_2 = np.transpose(theta1) * a_1
         print(z_2.shape)
         a_2 = sigmoid(z_2)
-        a_2 = np.concatenate((np.ones((m, 1)), a_2))
-        z_3 = np.multiply(theta2, a_2)
+        a_2 = np.concatenate((np.ones((1, 1)), a_2), 0)
+        z_3 = np.transpose(theta2) * a_2
         a_3 = sigmoid(z_3)
-
+        
         y_log = np.transpose(range(labels) == y[t])
         del_3 = a_3 - y_log
         del_2 = np.multiply(theta2, del_3 * np.concatenate((np.ones((1, 1)), sigmoid_grad(z_2)), -1))
